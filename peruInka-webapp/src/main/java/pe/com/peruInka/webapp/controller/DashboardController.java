ba9cbd0254c72b9e909a57596581c6bed5610623
ocalsin <omarinccjcc@hotmail.com>
2016-04-10 23:44:10 -0500

    add new DashboarController Class

--- peruInka-webapp/src/main/java/pe/com/peruInka/webapp/controller/DashboardController.java
+++ peruInka-webapp/src/main/java/pe/com/peruInka/webapp/controller/DashboardController.java
@@ -1,5 +1,8 @@
 package pe.com.peruInka.webapp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -16,38 +19,28 @@ public class DashboardController {
 
 	@RequestMapping(value = "home/dashboard", method = RequestMethod.GET)
 	public String electionsDashboard(Model model, HttpServletRequest request) {
-		// model.addAttribute("listElection", listElection);
+
+		Person person = new Person();
+		person.setName("Omar");
+		
+		List<Person> list=new ArrayList<Person>();
+		list.add(person);
+		
+		model.addAttribute("listPerson", list);
 
 		return "home/dashboard";
 	}
 
 	@RequestMapping(value = "/home/searchJsonById/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
-	public @ResponseBody Person searchJsonById(@PathVariable("id") Long id) {
-		
-		Person person= new Person();
-		
-		person.setId(id);
-		person.setDni("42114381");
-		person.setName("Omar");
-		person.setLastNameP("Calsin");
-		person.setLastNameM("Curo");
-		
-		return person;
+	public @ResponseBody String searchJsonById(@PathVariable("id") Long id) {
+
+		return "Return value::: " + id;
 	}
 
-	
 	@RequestMapping(value = "/home/searchXMLById/{id}", method = RequestMethod.GET, headers = "Accept=application/xml")
-	public @ResponseBody Person searchXMLById(@PathVariable("id") Long id) {
-		
-		Person person= new Person();
-		
-		person.setId(id);
-		person.setDni("42114381");
-		person.setName("Omar");
-		person.setLastNameP("Calsin");
-		person.setLastNameM("Curo");
-		
-		return person;
+	public @ResponseBody String searchXMLById(@PathVariable("id") Long id) {
+
+		return "Return value::: " + id;
 	}
-	
+
 }
--- peruInka-webapp/src/main/webapp/WEB-INF/jsp/home/dashboard.jsp
+++ peruInka-webapp/src/main/webapp/WEB-INF/jsp/home/dashboard.jsp
@@ -1,3 +1,21 @@
+<%@ page import="org.springframework.web.util.UrlPathHelper"%>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+<%--@ taglib uri="http://java.sun.com/jstl/fmt" prefix="fmt" --%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<%@ taglib uri="http://displaytag.sf.net" prefix="display" %>
+<%@ taglib uri="http://www.opensymphony.com/sitemesh/decorator" prefix="decorator" %>
+
 dashboard
 
 Bienvenido a la cadena de hotel PeruInka
+
+<br>
+
+
+<display:table name="${listPerson}" id="item" class="table">
+ 	<display:column title="Nombres y Apellidos" property="name" />
+</display:table>
